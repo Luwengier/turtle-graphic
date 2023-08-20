f2fde3906acd377f7503a35a54fdbe0023d16ef3
Luewngier <g217881111@gmail.com>
2023-08-20 23:18:13 +0800

    Complete challenge 1 - draw a square

--- main.py
+++ main.py
@@ -4,8 +4,11 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("darkSeaGreen3")
 
-timmy_the_turtle.left(90)
-timmy_the_turtle.forward(100)
+# TODO: Draw a square
+
+for i in range(4):
+    timmy_the_turtle.right(90)
+    timmy_the_turtle.forward(100)
 
 
 screen = Screen()
