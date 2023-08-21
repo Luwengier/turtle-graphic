3cbb735ee7b88d788bd27e27d20f9e9c47f8b422
Luewngier <g217881111@gmail.com>
2023-08-21 23:59:28 +0800

    Complete challenge 3 - draw different shapes

--- main.py
+++ main.py
@@ -12,12 +12,21 @@ timmy_the_turtle.color("darkSeaGreen3")
 
 # TODO: Draw a dashed line
 
-for _ in range(8):
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.pendown()
+# for _ in range(8):
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.pendown()
+
+# TODO: Draw different shapes
+
+
+for i in range(3, 11):
+    for _ in range(i):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(360 / i)
 
 
 screen = Screen()
+screen.colormode(255)
 screen.exitonclick()
