2d0da41aa7576a62cf80b81f55bfa481b4658980
Luewngier <g217881111@gmail.com>
2023-08-21 23:22:13 +0800

    Complete challenge 2 - draw a dashed line

--- main.py
+++ main.py
@@ -6,9 +6,17 @@ timmy_the_turtle.color("darkSeaGreen3")
 
 # TODO: Draw a square
 
-for i in range(4):
-    timmy_the_turtle.right(90)
-    timmy_the_turtle.forward(100)
+# for i in range(4):
+#     timmy_the_turtle.right(90)
+#     timmy_the_turtle.forward(100)
+
+# TODO: Draw a dashed line
+
+for _ in range(8):
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.pendown()
 
 
 screen = Screen()
