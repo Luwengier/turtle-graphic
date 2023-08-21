d883e1692df15285b5c7f072f3fbb192885fd4ca
Luewngier <g217881111@gmail.com>
2023-08-22 00:07:48 +0800

    Update challenge 3 with random pen color

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from turtle import Turtle, Screen
+from random import randint
+
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("darkSeaGreen3")
+
+screen = Screen()
+screen.colormode(255)
+
 
 # TODO: Draw a square
 
@@ -20,13 +26,11 @@ timmy_the_turtle.color("darkSeaGreen3")
 
 # TODO: Draw different shapes
 
-
 for i in range(3, 11):
+    timmy_the_turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
     for _ in range(i):
         timmy_the_turtle.forward(100)
         timmy_the_turtle.right(360 / i)
 
 
-screen = Screen()
-screen.colormode(255)
 screen.exitonclick()
