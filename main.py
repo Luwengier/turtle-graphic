b92819906014008d95bfa81c77552731803a3922
Luewngier <g217881111@gmail.com>
2023-08-23 03:34:05 +0800

    Complete challenge 4 - draw a random walk

--- main.py
+++ main.py
@@ -26,11 +26,26 @@ screen.colormode(255)
 
 # TODO: Draw different shapes
 
-for i in range(3, 11):
+# for i in range(3, 11):
+#     timmy_the_turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+#     for _ in range(i):
+#         timmy_the_turtle.forward(100)
+#         timmy_the_turtle.right(360 / i)
+
+# TODO: Draw a random walk
+
+timmy_the_turtle.speed(9)
+timmy_the_turtle.pensize(15)
+
+
+def random_walk():
     timmy_the_turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-    for _ in range(i):
-        timmy_the_turtle.forward(100)
-        timmy_the_turtle.right(360 / i)
+    timmy_the_turtle.setheading(90 * randint(0, 3))
+    timmy_the_turtle.forward(30)
+
+
+for _ in range(72):
+    random_walk()
 
 
 screen.exitonclick()
