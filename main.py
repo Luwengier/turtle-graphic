795931afa04884c0fb4c0158a6dc48fcd50cd05e
Luewngier <g217881111@gmail.com>
2023-08-25 16:53:33 +0800

    Complete final challenge - the hirst painting project

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
 from turtle import Turtle, Screen
 from random import randint
+import colorgram
 
+
+colors = colorgram.extract("hirst-spot.jpg", 20)
+colorLen = len(colors)
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.speed(10)
+timmy_the_turtle.speed("fastest")
 timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("darkSeaGreen3")
+timmy_the_turtle.pu()
+timmy_the_turtle.goto(-315, -315)
 
 screen = Screen()
 screen.colormode(255)
+
+
+def pick_color():
+    return colors[randint(2, colorLen - 1)].rgb
+
+
+for _ in range(100):
+    timmy_the_turtle.dot(20, pick_color())
+
+    if _ == 99:
+        continue
+    elif (_ + 1) % 10 == 0:
+        timmy_the_turtle.setheading(90)
+        timmy_the_turtle.forward(70)
+        if (_ + 1) % 20 == 0:
+            timmy_the_turtle.setheading(0)
+        else:
+            timmy_the_turtle.setheading(180)
+    else:
+        timmy_the_turtle.forward(70)
+
+screen.exitonclick()
 
 
 # TODO: Draw a square
@@ -50,14 +77,11 @@ screen.colormode(255)
 # TODO: Draw a spirograph
 
 
-def draw_spirograph(num: int):
-    for _ in range(num):
-        timmy_the_turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
-        timmy_the_turtle.circle(50)
-        timmy_the_turtle.left(360 / num)
+# def draw_spirograph(num: int):
+#     for _ in range(num):
+#         timmy_the_turtle.pencolor(randint(0, 255), randint(0, 255), randint(0, 255))
+#         timmy_the_turtle.circle(50)
+#         timmy_the_turtle.left(360 / num)
 
 
-draw_spirograph(18)
-
-
-screen.exitonclick()
+# draw_spirograph(18)
